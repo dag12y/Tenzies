e6bf18c812bafc96d9db67aae2bfcffcac2efaa8
dagm <dagimyibabe19@gmail.com>
2025-07-14 21:15:13 +0300

    adding game won features

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import Die from "./Die"
 import { useState } from "react"
 import {nanoid} from 'nanoid'
+import ReactConfetti from "react-confetti"
+
 function App() {
   const [dice,setDice]=useState(generateAllNewDIce())
+
+  const gameWon = dice.every(die => die.isHeld) && 
+        dice.every(die => die.value === dice[0].value)
 
   function generateAllNewDIce(){
     return new Array(10)
@@ -40,12 +45,16 @@ function App() {
   />)
   return(
     <main>
+      {gameWon && <ReactConfetti />}
+
       <h1 className="title">Tenzies</h1>
       <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
       <div className="dice-container">
         {diecElements}
       </div>
-      <button className='roll-dice' onClick={rollDice}>Roll</button>
+      <button className='roll-dice' onClick={rollDice}>
+        {gameWon ? "New Game" : "Roll"}
+      </button>
     </main>
   )
 }
