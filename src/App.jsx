2760d9ce63fdae70fbc1414cb11cdc921a2ae066
dagm <dagimyibabe19@gmail.com>
2025-07-14 17:50:40 +0300

    adding a roll dice button and conver the random number array to object

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,29 @@
 import Die from "./Die"
 import { useState } from "react"
+import {nanoid} from 'nanoid'
 function App() {
   const [dice,setDice]=useState(generateAllNewDIce())
 
   function generateAllNewDIce(){
     return new Array(10)
                 .fill(0)
-                .map(()=>(Math.ceil(Math.random()*6)))
+                .map(()=>({
+                  value:(Math.ceil(Math.random()*6)),
+                  isHeld:false,
+                  id:nanoid()
+                }))
   }
-  const diecElements =dice.map(value=><Die value={value}/>)
+
+  function rollDice(){
+    setDice(generateAllNewDIce())
+  }
+  const diecElements =dice.map(dieObj=><Die key={dieObj.id} value={dieObj.value}/>)
   return(
     <main>
       <div className="dice-container">
         {diecElements}
       </div>
+      <button className='roll-dice' onClick={rollDice}>Roll</button>
     </main>
   )
 }
