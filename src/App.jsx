6b270db5237e12029bbe20c58e932f2ef522b45f
dagm <dagimyibabe19@gmail.com>
2025-07-14 20:46:42 +0300

    work on isheld property to freeze the die from rolling and highlighting it

--- src/App.jsx
+++ src/App.jsx
@@ -14,12 +14,34 @@ function App() {
                 }))
   }
 
-  function rollDice(){
-    setDice(generateAllNewDIce())
+  function hold(id){
+    setDice(oldDice=>oldDice.map(die=>
+        die.id === id ?
+                  {...die, isHeld: !die.isHeld} :
+                  die
+    ))
   }
-  const diecElements =dice.map(dieObj=><Die key={dieObj.id} value={dieObj.value}/>)
+
+  function rollDice(){
+
+    setDice(oldDice=>
+      oldDice.map(die=>
+        die.isHeld ?
+        die : 
+        {...die,value:Math.ceil(Math.random()*6)}
+      )
+    )
+  }
+  const diecElements =dice.map(dieObj=>
+  <Die key={dieObj.id}
+      value={dieObj.value}
+      isHeld={dieObj.isHeld}
+      hold={()=>{hold(dieObj.id)}}
+  />)
   return(
     <main>
+      <h1 className="title">Tenzies</h1>
+      <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
       <div className="dice-container">
         {diecElements}
       </div>
